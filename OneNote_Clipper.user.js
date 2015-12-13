19e941625f4a0de78cab5489eb6ce969c9514056
vylet <vy@live.in>
2015-12-13 14:13:32 +0530

    no message

---
+++ OneNote_Clipper.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name        OneNote Clipper
+// @namespace   https://github.com/vylet/OneNote
+// @description The clipper Bookmarklet
+// @include			http://*
+// @include			https://*
+// @version     1.0
+// @grant       none
+// ==/UserScript==
+
+var onenote = function() {
+        var jsCode = document.createElement('script');
+        jsCode.setAttribute('src', 'https://www.onenote.com/clipper/root?ClipperId=ON-6a191eef-84a2-4491-9cc2-2e4560cfb9af&ClipperType=Bookmarklet&ClipperVersion=2.0.2');
+        jsCode.setAttribute('id', 'oneNoteCaptureRootScript');
+        jsCode.setAttribute('type', 'text/javascript');
+        document.body.appendChild(jsCode);
+    };
+(function(){
+document.addEventListener('keydown', function(e) {
+  // pressed ctrl+alt+o
+  if (e.keyCode == 79 && !e.shiftKey && e.ctrlKey && e.altKey && !e.metaKey) {
+	  console.log("Clipper Hotkey")
+  onenote(); // Open OneNote Clipper.
+  }
+}, false);
+})();
